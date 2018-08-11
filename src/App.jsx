f11a05df33cf02da88ca75c162e90a2aff343f82
arju88nair <arju88nair@gmail.com>
2018-08-11 20:01:19 +0530

    Framework

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="App-title">Fuck you</div>
+                <div>
+                    <input placeholder="Search for the fucker here"></input>
+                    <button>Button</button>
+                </div>
+                <div className="profile">
+                    <div>Artist Name</div>
+                    <div>Artist Picture</div>
+                </div>
+                <div className="gallery">
+                    Galerry
+                </div>
+            </div>
+
+        )
+
+    };
+}
