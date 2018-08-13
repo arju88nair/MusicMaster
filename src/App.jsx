81a42d917e75a7320750cf56c2aa93021dbc6c7f
arju88nair <arju88nair@gmail.com>
2018-08-13 20:42:58 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,19 @@ export default class App extends Component {
     }
 
     search() {
+        var bearer='Bearer BQDsxMZUm77r0pVLCdYtA_qzTbwsI2ypndHu9WP-f94mUz-S4RJojz2XshS5xSUc1rzJrw_0lxbEXFscZm3c2HAlEeAJg2Jyzp1z8EVdGr0vAR0Y-tq3uMkQf7DcDSnPK50kREA7wL7i6Sjj4JfBNeebE0J9OwiRjiwxKDw1G8WK4c9f-pPw4wNXGoMPKwgSzKw5WOtL4tHIu-K74Q'
         const API_URI="https://api.spotify.com/v1/search?";
-        const FETCH_URI=API_URI+ "&q=" + this.state.query+"&type=artsist&limit=1"
+        const FETCH_URI=`${API_URI}q=${this.state.query}&type=artist&limit=1`;
+        fetch(FETCH_URI,{
+            method:'GET',
+            headers:new Headers({
+                'Authorization': bearer, 
+              }), 
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(error =>console.log(error));
+
     }
     render() {
         return (
